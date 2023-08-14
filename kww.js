19531e3afa1502851c7b76bda160ee5f0683f61f
mrdeer <70961031+wangbo777@users.noreply.github.com>
2023-08-14 17:05:53 +0800

    Update kww.js
    口味王小程序

--- kww.js
+++ kww.js
@@ -5,6 +5,7 @@
 ===================quantumultx================
 [task_local]
 #口味王小程序
+cron: 0  12 * *  * 
 0  12 * *  * kww.js, tag=口味王小程序-全套任务, enabled=true
 运行前安装node依赖 silly-datetime      cheerio      node-jsencrypt
 export kwwCookies="memberId&memberId&memberId" 
